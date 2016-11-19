7a9ba71421797dfe7615995de9b758590a9744ec
jonathan.deng <dengjonathan@gmail.com>
2016-11-19 11:59:54 -0500

    completed monad exes

--- code/part2_exercises/exercises/monads/monad_exercises.js
+++ code/part2_exercises/exercises/monads/monad_exercises.js
@@ -1,12 +1,14 @@
 require('../../support');
 var Task = require('data.task');
 var _ = require('ramda');
+var path = require('path');
 
-// Exercise 1
-// ==========
-// Use safeProp and map/join or chain to safely get the street name when given a user
+// Exercise 1 ========== Use safeProp and map/join or chain to safely get the
+// street name when given a user
 
-var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
+var safeProp = _.curry(function (x, o) {
+  return Maybe.of(o[x]);
+});
 var user = {
   id: 2,
   name: "albert",
@@ -18,60 +20,61 @@ var user = {
   }
 };
 
-var ex1 = undefined;
+var ex1 = _.compose(chain(safeProp('name')), chain(safeProp('street')), safeProp('address'));
 
+// Exercise 2 ========== Use getFile to get the filename, remove the directory
+// so it's just the file, then purely log it.
 
-// Exercise 2
-// ==========
-// Use getFile to get the filename, remove the directory so it's just the file, then purely log it.
-
-var getFile = function() {
-  return new IO(function(){ return __filename; });
+var getFile = function () {
+  return new IO(function () {
+    return __filename;
+  });
 }
 
-var pureLog = function(x) {
-  return new IO(function(){
+var pureLog = function (x) {
+  return new IO(function () {
     console.log(x);
     return 'logged ' + x; // for testing w/o mocks
   });
 }
 
-var ex2 = undefined;
+var ex2 = _.compose(chain(_.compose(pureLog, _.last, split(path.sep))), getFile);
 
+// Exercise 3 ========== Use getPost() then pass the post's id to getComments().
 
-
-// Exercise 3
-// ==========
-// Use getPost() then pass the post's id to getComments().
-
-var getPost = function(i) {
+var getPost = function (i) {
   return new Task(function (rej, res) {
     setTimeout(function () {
-      res({ id: i, title: 'Love them tasks' }); // THE POST
+      res({id: i, title: 'Love them tasks'}); // THE POST
     }, 300);
   });
 }
 
-var getComments = function(i) {
+var getComments = function (i) {
   return new Task(function (rej, res) {
     setTimeout(function () {
-      res([{post_id: i, body: "This book should be illegal"}, {post_id: i, body:"Monads are like smelly shallots"}]);
+      res([
+        {
+          post_id: i,
+          body: "This book should be illegal"
+        }, {
+          post_id: i,
+          body: "Monads are like smelly shallots"
+        }
+      ]);
     }, 300);
   });
 }
 
-var ex3 = undefined;
+var ex3 = _.compose(chain(_.compose(getComments, _.prop('id'))), getPost);
 
-
-// Exercise 4
-// ==========
-// Use validateEmail, addToMailingList and emailBlast to implement ex4's type signature.
-// It should safely add a new subscriber to the list, then email everyone with this happy news.
-
-//  addToMailingList :: Email -> IO [Email]
-var addToMailingList = (function(list){
-  return function(email) {
-    return new IO(function(){
+// Exercise 4 ========== Use validateEmail, addToMailingList and emailBlast to
+// implement ex4's type signature. It should safely add a new subscriber to the
+// list, then email everyone with this happy news.  addToMailingList :: Email ->
+// IO [Email]
+var addToMailingList = (function (list) {
+  return function (email) {
+    return new IO(function () {
       list.push(email);
       return list;
     });
@@ -80,18 +83,25 @@ var addToMailingList = (function(list){
 
 //  emailBlast :: [Email] -> IO String
 function emailBlast(list) {
-  return new IO(function(){
+  return new IO(function () {
     return 'emailed: ' + list.join(','); // for testing w/o mocks
   });
 }
 
 //  validateEmail :: Email -> Either String Email
-var validateEmail = function(x){
-  return x.match(/\S+@\S+\.\S+/) ? (new Right(x)) : (new Left('invalid email'));
+var validateEmail = function (x) {
+  return x.match(/\S+@\S+\.\S+/)
+    ? (new Right(x))
+    : (new Left('invalid email'));
 }
 
 //  ex4 :: Email -> Either String (IO String)
-var ex4 = undefined;
+var ex4 = _.compose(_.map(_.compose(chain(emailBlast), addToMailingList)), validateEmail);
 
-
-module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, user: user}
+module.exports = {
+  ex1: ex1,
+  ex2: ex2,
+  ex3: ex3,
+  ex4: ex4,
+  user: user
+}
